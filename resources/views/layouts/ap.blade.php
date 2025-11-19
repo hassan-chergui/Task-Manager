0fcd33fd3d80c1f983f0cbd087b935ac4e12d8b4
hassan-chergui <hassan.chergui95@gmail.com>
2025-11-19 12:31:09 +0100

    added breeze

--- resources/views/layouts/ap.blade.php
+++ resources/views/layouts/ap.blade.php
@@ -6,9 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>@yield('title')</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+     
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    @include('partials.navbar')
+    {{-- @include('partials.navbar') --}}
+    @include('layouts.navigation')
+    {{-- @extends('layouts.navigation') --}}
     <main class="container mt-4">
         @yield('content')
     </main>
