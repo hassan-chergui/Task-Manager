7ac933a8984208787d270d1f644c789bf5436d72
hassan-chergui <hassan.chergui95@gmail.com>
2025-11-19 11:18:43 +0100

    breeze2

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', [TaskController::class,'home'] );
-Route::get('/about', function(){
-    return view('about');
-} );
-Route::get('/contact', function(){
-    return view('contact');
-} );
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('tasks', TaskController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
