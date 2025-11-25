f14de877a5e20787bdeb2c6de3dccefd549a6f7c
hassan-chergui <hassan.chergui95@gmail.com>
2025-11-25 12:52:19 +0100

    feat(auth): implement user authentication with Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
 use App\Http\Controllers\Api\TaskController as ApiTaskController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
+// Public routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('tasks', ApiTaskController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('tasks', ApiTaskController::class);
+});
+
+
+
